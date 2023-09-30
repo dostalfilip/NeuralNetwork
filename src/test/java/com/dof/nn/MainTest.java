cc19ca28d7c3c8ff086fabc50f67708b0212e317
Dostal Filip <Filip.Dostal@airbank.cz>
2023-09-30 14:58:14 +0200

    init junit tests

---
+++ src/test/java/com/dof/nn/MainTest.java
@@ -0,0 +1,15 @@
+package com.dof.nn;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+
+    @Test
+    void test(){
+        assertEquals(1, 1);
+    }
+
+}
