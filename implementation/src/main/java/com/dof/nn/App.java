c07076787ed5be7f538e6e3723d3475385b3d665
Dostal Filip <Filip.Dostal@airbank.cz>
2023-11-05 18:52:12 +0000

    NeuralNetwork

--- implementation/src/main/java/com/dof/nn/App.java
+++ implementation/src/main/java/com/dof/nn/App.java
@@ -2,9 +2,12 @@ package com.dof.nn;
 
 import com.dof.nn.core.NeuralNetwork;
 import com.dof.nn.core.Transform;
+import com.dof.nn.loader.Loader;
+import com.dof.nn.loader.test.TestLoader;
 
 public class App {
     public static void main(String[] args) {
+
         int inputRows = 10;
         int outputRows = 3;
 
@@ -13,6 +16,14 @@ public class App {
         neuralNetwork.add(Transform.RELU);
         neuralNetwork.add(Transform.DENSE, outputRows);
         neuralNetwork.add(Transform.SOFTMAX);
+
+        neuralNetwork.setEpochs(20);
+        neuralNetwork.setLearningRates(0.02, 0);
+
+        Loader trainLoader = new TestLoader(60_000, 32);
+        Loader testLoader = new TestLoader(10_000, 32);
+
+        neuralNetwork.fit(trainLoader, testLoader);
 
         System.out.println(neuralNetwork);
     }
--- implementation/src/main/java/com/dof/nn/core/NeuralNetwork.java
+++ implementation/src/main/java/com/dof/nn/core/NeuralNetwork.java
@@ -1,7 +1,14 @@
 package com.dof.nn.core;
 
+import com.dof.nn.loader.Loader;
+
 public class NeuralNetwork {
     private Engine engine;
+
+    private int epochs = 20;
+    private double learningRate;
+    private double initialLearningRate = 0.01;
+    private double finalLearningRate = 0;
 
     public NeuralNetwork() {
         this.engine = new Engine();
@@ -9,6 +16,48 @@ public class NeuralNetwork {
 
     public void add(Transform transform, double... params) {
         engine.add(transform, params);
+    }
+
+    public void setLearningRates(double initialLearningRate, double finalLearningRate) {
+        this.initialLearningRate = initialLearningRate;
+        this.finalLearningRate = finalLearningRate;
+    }
+
+    public void setEpochs(int epochs) {
+        this.epochs = epochs;
+    }
+
+    public void fit(Loader trainLoader, Loader evalLoader) {
+        learningRate = initialLearningRate;
+
+        for (int epoch = 0; epoch < epochs; epoch++) {
+            System.out.printf("Epoch %3d ", epoch);
+
+            runEpoch(trainLoader, true);
+
+            if (evalLoader != null) {
+                runEpoch(evalLoader, false);
+            }
+
+            learningRate -= (initialLearningRate - finalLearningRate) / epoch;
+        }
+
+    }
+
+    private void runEpoch(Loader loader, boolean trainingMode) {
+        loader.open();
+
+        var queue = createBatchTasks(loader, trainingMode);
+        consumeBatchTasks(queue, trainingMode);
+
+        loader.close();
+    }
+
+    private void consumeBatchTasks(Object queue, boolean trainingMode) {
+    }
+
+    private Object createBatchTasks(Loader loader, boolean trainingMode) {
+        return null;
     }
 
     @Override
